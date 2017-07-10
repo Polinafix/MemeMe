d50e6b1b70275fbc1dcf35bdec8bf1d46a471354
polinafiksson <pd.fiksson@gmail.com>
2017-07-09 19:06:15 -0500

    hide and unhide rab bar in the detail view

--- MemeMe/MemeDetailViewController.swift
+++ MemeMe/MemeDetailViewController.swift
@@ -18,6 +18,15 @@ class MemeDetailViewController: UIViewController {
         detailedImage.image = imageToShow
 
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.tabBarController?.tabBar.isHidden = false
+    }
 
    
     
